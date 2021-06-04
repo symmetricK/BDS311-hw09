e9c0b02f1dcdadcd3e7600094733c6b9bef1c24b
Kei Horikawa <symmetrick5211@gmail.com>
2021-06-04 13:38:12 -0700

    created evaluate_accuracy function

--- knn_fxns.py
+++ knn_fxns.py
@@ -58,4 +58,18 @@ def classify(df, example_row, k):
     majority_class=sorted_df['class'].iloc[0]
     return majority_class
 
-def evaluate_accuracy
+def evaluate_accuracy(test_df,training_df1,training_df2,k):
+    '''Returns the proportion of the test_df that was correctly classfied'''
+    test_attribute_df=test_df.drop(['class'],axis=1)
+    
+    training_dataframes=[training_df1,training_df2]
+    combined_training_df=pd.concat(training_dataframes)
+    num_correct=0
+    num_rows=test_df.shape[0]
+    
+    for num_row in np.arange(num_rows):
+        example_row=test_attribute_df.iloc[num_row,:]
+        c=classify(combined_training_df,example_row,k)
+        if c==test_df['class'].iloc[num_row]:
+            num_correct=num_correct+1
+    return num_correct/num_rows
